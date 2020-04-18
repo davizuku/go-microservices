4dc0fb93a39a4ab450f1e8c5c74b87fb23f4d8ca
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-18 17:32:21 +0200

    Add graceful shutdown

--- src/main.go
+++ src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -16,5 +19,28 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":3000", sm)
+	// Non standard server creation. Standard server is http package.
+	s := &http.Server{
+		Addr:         ":3000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Graceful shutdown configuration
+	// @see https://golang.org/pkg/os/signal/#example_Notify
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	timeoutCtxt, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(timeoutCtxt)
 }
