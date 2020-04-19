3509d822cd55fa3828eb6ad298237eb42923262e
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-19 17:06:09 +0200

    Add PUT endpoint

--- src/handlers/products.go
+++ src/handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -22,6 +24,23 @@ func (p *Products) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
 		p.addProduct(res, req)
 		return
+	}
+	if req.Method == http.MethodPut {
+		p.l.Println("Parse PUT Product URL")
+		// expect the id to be in the URI
+		regex := regexp.MustCompile(`/([0-9]+)`)
+		groups := regex.FindAllStringSubmatch(req.URL.Path, -1)
+		if len(groups) != 1 || len(groups[0]) != 2 {
+			http.Error(res, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := groups[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(res, "Could not convert string ID to int", http.StatusBadRequest)
+			return
+		}
+		p.updateProducts(id, res, req)
 	}
 	res.WriteHeader(http.StatusMethodNotAllowed)
 }
@@ -44,4 +63,23 @@ func (p *Products) addProduct(res http.ResponseWriter, req *http.Request) {
 	}
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, res http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handle PUT Product")
+	prod := &data.Product{}
+	err := prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(res, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(res, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(res, "Could not update the product", http.StatusInternalServerError)
+		return
+	}
+	p.l.Printf("Prod: %#v", prod)
 }
